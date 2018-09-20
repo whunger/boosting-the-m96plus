98875898673eeda8b0c66e10c8d0371bb1886b95
Werner Hunger <woe@gmx.de>
2018-09-20 23:58:53 +0200

    Added rescaled boot animation

--- system/install-config.sh
+++ system/install-config.sh
@@ -4,7 +4,9 @@ mount -o remount,rw /system
 busybox tar xzvf files-config.tgz -C /system
 
 chmod 644 /system/build.prop
+chmod 644 /system/media/bootanimation.zip
 
 chown 0:0 /system/build.prop
+chown 0:0 /system/media/bootanimation.zip
 
 mount -o remount,ro /system
