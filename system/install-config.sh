f709e4f8c2f61ef571556c2e21e4bb225691193b
Werner Hunger <woe@gmx.de>
2018-09-22 18:06:00 +0200

    Added standby/suspend helper daemon

--- system/install-config.sh
+++ system/install-config.sh
@@ -5,8 +5,10 @@ busybox tar xzvf files-config.tgz -C /system
 
 chmod 644 /system/build.prop
 chmod 644 /system/media/bootanimation.zip
+chmod 755 /system/xbin/sleepd
 
 chown 0:0 /system/build.prop
 chown 0:0 /system/media/bootanimation.zip
+chown 0:0 /system/xbin/sleepd
 
 mount -o remount,ro /system
