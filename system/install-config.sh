6a0e917675c20e4afc1f55cd67bf0d8895930ece
Werner Hunger <woe@gmx.de>
2020-04-23 09:51:11 +0200

    Added certificates

--- system/install-config.sh
+++ system/install-config.sh
@@ -7,9 +7,11 @@ chmod 755 $1/media
 chmod 644 $1/media/bootanimation.zip
 chmod 755 $1/xbin
 chmod 755 $1/xbin/sleepd
+chmod 644 $1/etc/security/cacerts/*
 
 chown 0:0 $1/build.prop
 chown 0:0 $1/media
 chown 0:0 $1/media/bootanimation.zip
 chown 0:2000 $1/xbin
 chown 0:0 $1/xbin/sleepd
+chown 0:0 $1/etc/security/cacerts/*
